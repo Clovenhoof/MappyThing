9d434c1d11479994f1f90da2924b39eb17c7cfe0
Jean Nygård <jean@webbhuset.fi>
2019-12-03 15:19:25 +0200

    delete confirm, fix naming convention

--- src/php/Templates/delete.php
+++ src/php/Templates/delete.php
@@ -1,6 +1,6 @@
 <h2>Delete location</h1>
 <p>Really delete the location '<?= $title; ?>'?</p>
 <div class="functions">
-    <a class="cancel">Cancel</a>
-    <a class="save">Ok</a>
+    <a class="fn-cancel">Cancel</a>
+    <a class="fn-save">Ok</a>
 </div>
