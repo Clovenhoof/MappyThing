ef7f9fb7ddfd1171b77fca26d4c6305d06595ea0
Jean Nygård <jean@webbhuset.fi>
2019-12-04 04:26:38 +0200

    Templates/main, add filter dummies

--- src/php/Templates/main.php
+++ src/php/Templates/main.php
@@ -9,10 +9,24 @@
     <body>
         <main>
             <div class="toolset">
-                <a class="fn-add" aria-title="Add stuff">
+                <a class="fn fn--add" aria-label="Add stuff">
                     <span class="material-icons">add</span>
                     <span class="text">Add location</span>
                 </a>
+                <div class="filters">
+                    <a class="fn fn--open" aria-label="Filter open places">
+                        <span class="material-icons">timelapse</span>
+                        <span class="text">Currently open</span>
+                    </a>
+                    <a class="fn fn--favorites" aria-label="Filter favorites">
+                        <span class="material-icons">favorite_outline</span>
+                        <span class="text">Show favorites</span>
+                    </a>
+                </div>
+                <div class="filters">
+                    <select class="fn--keywords"></select>
+                    <input placeholder="Search" type="text" class="fn fn--search">
+                </div>
             </div>
             <div class="map"></div>
         </main>
